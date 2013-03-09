61d945d1aba2b9aaca6b7206d52db4d6d99f2801
Mark Provenzano <kj.provenzano@gmail.com>
2013-03-09 17:15:48 -0500

    BlackJack game is working but buggy
    moved functionality to Program.java

--- Fun/src/MiniGames/BlackJack.java
+++ Fun/src/MiniGames/BlackJack.java
@@ -7,47 +7,84 @@ public class BlackJack {
 	private static Scanner scan = new Scanner(System.in);
 	private static Random rand = new Random();
 	
-	public static void main(String[] args) {
+	public void start() {
+		System.out.println("Welcome to Black Jack");
+		System.out.println("Remember dealer always wins in ties and you loose if you go over 21");
 		
 		int dealer = dealer();
 		int player = player();
-		getResult(dealer, player);
+		
+		int dealer1[] = dealerConversion(dealer);
+		
+		int real = dealer1[0];
+		int convert = dealer1[1];
+		
+		getResult(real, player, convert);
+	}
+	
+	public int[] dealerConversion(int dealer) {
+		
+		int real;
+		int convert;
+		
+		if(dealer > 21) {
+			real = dealer;
+			convert = 0;
+		return new int[] {real, convert};
+		}
+		else {
+			real = dealer;
+			convert = dealer;
+		return new int[]{dealer, convert};
+		}
 	}
 	
 	public static int dealer() {
 		int x = rand.nextInt(11) + 1;
+		
 		do {
 			x = x + rand.nextInt(11) + 1;
 		} while(x < 16);
-		return x;
+
+			return x;
 	}
 	
 	public static int player() {
 		int y = 0;
-		String hit;
+		String hit = "hit";
 		
-		do {
+		while(y <= 21 && hit.equals("hit")) {
 			y = y + rand.nextInt(11) + 1;
-			if(y == 11) {
-				System.out.println("You pulled an ace use as 1 or ll?");
-				y = scan.nextInt();
-			}
 			
+			if (y >= 21) {
+				hit = "stay";
+			}
+			else {
 			System.out.println("You have " + y);
 			System.out.println("Hit or Stay?");
 			hit = scan.nextLine();
-		} while (y < 21 && hit.equals("hit"));
+			}
+		}
 		
 		return y;
 	}
 	
-	public static void getResult(int dealer , int player) {
-		if(dealer == 21 && player == 21)
+	
+	public static void getResult(int dealer , int player , int conversion) {
+		if(conversion == 21 && player == 21)
 			System.out.println("You lost, the dealer has 21");
-		else if(dealer > player)
+		else if(conversion > player && player <= 21)
 			System.out.println("You lost the dealer had " + dealer);
-		else if(dealer < player)
-			System.out.println("YOU WON!");
+		else if(conversion < player && player <= 21)
+			System.out.println("YOU WON!\n You had " + player + " Dealer had " + dealer);
+		else if(conversion > 21 && player > 21) {
+			System.out.println("You lost!\n You had " + player + " Dealer had " + dealer);
+		}
+		else if(player == conversion) {
+			System.out.println("You lost!\n You had " + player + " Dealer had " + dealer);
+		}
+		else
+			System.out.println("You lost you went over 21. you had " + player + " Dealer had " + dealer);
 	}
 
 }
--- Fun/src/MiniGames/Program.java
+++ Fun/src/MiniGames/Program.java
@@ -1,13 +1,43 @@
 package MiniGames;
 
+import java.util.Scanner;
+
 public class Program {
-
-	/**
-	 * @param args
-	 */
+	public static BlackJack bGame = new BlackJack();
+	public static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		menu();
+
+	}
+	
+	public static void menu() {
+		String choice;
+		int c = 1;
+		
+		do {
+			System.out.println("Which game would you like to play?");
+			System.out.println("1: Black Jack");
+			System.out.println("2: Flip a Coin");
+				int x = scan.nextInt();
+				if(x == 1) {
+					bGame.start();
+				}
+				else if(x == 2) {
+					HeadsorTails.start();
+				}
+				else {
+					System.out.println(x + " is an invalid menu item");
+					menu();
+				}
+				System.out.println("Play again?");
+				choice = scan.next();
+				if(choice.equals("yes"))
+					c = 1;
+				else
+					c = 0;
+		} while(c != 0);
 	}
 
 }
