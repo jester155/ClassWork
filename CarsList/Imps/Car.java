9f8698864ae0f74d1b2ed10050502893da05b8e2
Mark Provenzano <kj.provenzano@gmail.com>
2013-03-10 03:09:08 -0400

    BEFORE TYLER
    SOOON WILL HAVE AFTER

---
+++ CarsList/Imps/Car.java
@@ -0,0 +1,33 @@
+package Imps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Car {
+	
+	public List<String> Cars = new ArrayList<String>();
+
+	public List<String> getAll() {
+
+		Cars.add(getModel());
+		Cars.add(getReg());
+		
+		return Cars;
+	}
+	
+	public String getModel() {
+		String model = "Chev";
+		
+		return model;
+	}
+	
+	public String getReg() {
+		String reg;
+		
+		reg = "m87v67";
+		
+		return reg;
+	}
+	
+}
---
+++ CarsList/Imps/Program.java
@@ -0,0 +1,16 @@
+package Imps;
+
+
+public class Program {
+
+	public static Car cars = new Car();	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(cars.getAll());
+		
+		System.out.println(cars.getReg());
+		
+	}
+
+}
